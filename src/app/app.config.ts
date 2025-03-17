79a3ddb33a91bc32950f3b807a0891ffa3f09db2
Tom Paschke <TomP@cccsw.local>
2025-03-17 11:41:15 +0100

    initial

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), {provide: MAT_DATE_LOCALE, useValue: 'de-DE'}]
-};
